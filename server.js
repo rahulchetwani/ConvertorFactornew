4a346aaf6fa4065175aabfc6903bbb3dce24c33c
rahulchetwani <37891855+rahulchetwani@users.noreply.github.com>
2018-03-29 11:34:09 +0530

    vdv
    dsvdsv

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var bodyParser=require('body-parser');
 var app=express();
-
+var port=process.env.PORT || 3000;
 app.get('/getCurrencyconvertorfactor',function(req,res)
 {
     //console.log(req.query);
     //res.send("Hello "+ req.query.name);
     res.json({result:64.75});
 });
-app.listen(3003);
+app.listen(port);
