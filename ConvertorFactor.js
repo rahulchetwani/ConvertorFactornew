6ae26ed62bc4d5220557c010a35fffb12bebc37e
rahulchetwani <37891855+rahulchetwani@users.noreply.github.com>
2018-03-29 10:36:44 +0530

    Initial commit

---
+++ ConvertorFactor.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var bodyParser=require('body-parser');
+var app=express();
+
+app.get('/getCurrencyconvertorfactor',function(req,res)
+{
+    //console.log(req.query);
+    //res.send("Hello "+ req.query.name);
+    res.json({result:64.72});
+});
+app.listen(3003);
